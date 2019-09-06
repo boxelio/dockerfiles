cf1ac519bf3c8e9fa7fbbea90e926ded8a811091
Kevin Elliott <kevin@welikeinc.com>
2019-09-05 22:58:59 -0700

    Update the build script for all

--- utils/build_and_push_all.sh
+++ utils/build_and_push_all.sh
@@ -1,2 +1,3 @@
 utils/build_and_push.sh chrony Dockerfile boxel/chrony:latest
-utils/build_and_push.sh rtl433 Dockerfile boxel/rtl433:latest
+utils/build_and_push.sh multimon-ng Dockerfile boxel/multimon-ng:latest
+utils/build_and_push.sh rtl_433 Dockerfile boxel/rtl_433:latest
