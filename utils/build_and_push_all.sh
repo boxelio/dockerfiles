33af6e021cb49337da6e59ad61ef5bab8e81c120
Kevin Elliott <kevin@welikeinc.com>
2019-09-06 00:17:56 -0700

    Add direwolf build to the build all script

--- utils/build_and_push_all.sh
+++ utils/build_and_push_all.sh
@@ -1,3 +1,4 @@
 utils/build_and_push.sh chrony Dockerfile boxel/chrony:latest
+utils/build_and_push.sh direwolf Dockerfile boxel/direwolf:latest
 utils/build_and_push.sh multimon-ng Dockerfile boxel/multimon-ng:latest
 utils/build_and_push.sh rtl_433 Dockerfile boxel/rtl_433:latest
