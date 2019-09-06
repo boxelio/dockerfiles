bf837481ccd31305e0594e1b4f733ad96b694542
Kevin Elliott <kevin@welikeinc.com>
2019-09-06 11:43:11 -0700

    Add gnuradio-pybombs-base to the build all

--- utils/build_and_push_all.sh
+++ utils/build_and_push_all.sh
@@ -1,6 +1,9 @@
+utils/build_and_push.sh gnuradio-pybombs-base Dockerfile boxel/gnuradio-pybombs-base:latest
+
 utils/build_and_push.sh chrony Dockerfile boxel/chrony:latest
 utils/build_and_push.sh direwolf Dockerfile boxel/direwolf:latest
 utils/build_and_push.sh multimon-ng Dockerfile boxel/multimon-ng:latest
 utils/build_and_push.sh npemwin Dockerfile boxel/npemwin:latest
 utils/build_and_push.sh rtl_433 Dockerfile boxel/rtl_433:latest
+utils/build_and_push.sh webwspr Dockerfile boxel/webwspr:latest
 utils/build_and_push.sh weewx Dockerfile boxel/weewx:latest
