3897c41b3e147e6aeb5b6d8470384b903079d60a
Kevin Elliott <kevin@welikeinc.com>
2019-10-03 02:08:58 -0700

    Improvements to the support scripts to simplify

--- support/scripts/build_and_push.sh
+++ support/scripts/build_and_push.sh
@@ -2,5 +2,8 @@
 
 cd $1
 # docker buildx build --file=$2 --platform=linux/amd64,linux/arm64,linux/arm/v7 -t $3 --push .
-docker buildx build --progress=plain --no-cache --file=$2 --platform=linux/amd64,linux/arm64,linux/arm/v7 -t $3 --push .
+# docker buildx build --progress=plain --no-cache --file=Dockerfile --platform=linux/amd64,linux/arm64,linux/arm/v7 -t boxel/$1:latest --push .
+docker buildx build --progress=plain --no-cache --file=Dockerfile --platform=linux/amd64,linux/arm/v7 -t boxel/$1:latest --push .
+# docker buildx build --progress=plain --file=$2 --platform=linux/amd64,linux/arm64,linux/arm/v7 -t $3 --push .
 # docker buildx build --file=$2 --platform=linux/amd64,linux/arm64,linux/arm/v7 -t $3 --push .
+# docker buildx build --progress=plain --no-cache --file=$2 -t $3 --push .
