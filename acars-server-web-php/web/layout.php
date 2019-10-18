16f589961ce6cd863d48798571f9425ca53d31d8
Kevin Elliott <kevin@welikeinc.com>
2019-10-17 17:27:00 -0700

    acars-server-web-php: Misc

--- acars-server-web-php/web/layout.php
+++ acars-server-web-php/web/layout.php
@@ -12,7 +12,7 @@
 <!DOCTYPE html>
 <html>
   <head>
-    <title><?php echo $title ?: 'ACARS' ?></title>
+    <title><?php echo $title ?? 'ACARS' ?></title>
     <link rel="stylesheet" type="text/css" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
   </head>
   <body>
@@ -35,7 +35,7 @@
     </nav>
 
     <div id="page">
-      <?php echo $content ?: '' ?>
+      <?php echo $content ?? '' ?>
     </div>
   </body>
 </html>
